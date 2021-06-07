2378b4c160810e622a330f540b3343cc31d925cd
s02180666 <s02180666@prac.cs.msu.ru>
2021-06-07 16:43:56 -0700

    ColorMap docstring documentation improvement

--- CellAutomate/colorMap.py
+++ CellAutomate/colorMap.py
@@ -1,8 +1,21 @@
 class ColorMap:
     """
-    -lifeMap resulting matrix
-    -Transfered to view.py
-    -Remains constant on single iteration
+    ColorMap class is a class that transfers fixed resulting matrix
+    retrieved from liveMap to view.py module
+
+    Attributes
+    ----------
+    lifeMap: class
+        an interactive matrix^ representing the current state of the system
+
+    Methods
+    -------
+    getSize()
+        retrieves the size of the class object
+    getColor(m,n)
+        retrieves the color of cell with coordinates (m,n)
+    getColorMatrix()
+        retrieves the "colored" matrix
     """
     def __init__(self, lifeMap):
         self._size = lifeMap.getSize()
