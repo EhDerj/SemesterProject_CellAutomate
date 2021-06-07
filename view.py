aeedb42d24f5c921a7c8dc2b1852d9b41f46b4e3
Rodion Kostin <itrodion@gmail.com>
2021-06-08 01:12:27 +0300

    view: Display map on canvas

--- view.py
+++ view.py
@@ -3,8 +3,11 @@ Tkinter skeleton app
 '''
 import tkinter as tk
 from CellAutomate.colors import Colors
+from collections import namedtuple
 
 
+MAP_SIZE = namedtuple('RectangleSize', ['width', 'height'])(50, 50)
+CELL_SIZE = 10
 colors = [color for color in Colors().colors][1:len(Colors().colors)//2]
 
 
@@ -51,15 +54,32 @@ class CellAutomateGUI(tk.Frame):
   def showMainWindow(self):
     '''Show main window.'''
 
+    # Init state
+    self.map = [[0] * MAP_SIZE.width for i in range(MAP_SIZE.height)]
+    self.iteration = 0
+
     # Set up window
     self.master.title('Клеточный мир!')
     self.destroyAllWidgets()
     
     # Set up widgets
-    self.cvsMap = tk.Canvas(self)
+    self.cvsCells = tk.Canvas(self, width=MAP_SIZE.width * CELL_SIZE, height=MAP_SIZE.height * CELL_SIZE)
     self.btnExit = tk.Button(self, text='Exit!', command=self.showWelcomeWindow)
-    self.cvsMap.grid()
+    self.cvsCells.grid()
     self.btnExit.grid()
 
+    self.refreshMap()
+    self.refreshScene()
+
+  def refreshScene(self):
+    '''Refresh cells.'''
+    for i, row in enumerate(self.map):
+      for j, colorIndex in enumerate(row):
+        x0, y0 = j * CELL_SIZE, i * CELL_SIZE
+        color = ['white', *colors][colorIndex]
+        self.cvsCells.create_rectangle(x0, y0, x0 + CELL_SIZE, y0 + CELL_SIZE, fill=color, outline='#eee')
   
+  def refreshMap(self):
+    self.iteration += 1
+    self.map = [[(i + j + self.iteration) % 2 for j in range(MAP_SIZE.width)] for i in range(MAP_SIZE.height)]
 
