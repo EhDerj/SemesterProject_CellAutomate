ccfa991e7a7e1242c1a4c01269f6f79b0697348d
Elizaveta Koblova <eriskob@gmail.com>
2021-06-07 17:24:49 +0300

    initial commit

---
+++ LifeMap.py
@@ -0,0 +1,56 @@
+class Colors:
+    
+
+    def __init__(self):
+        colors = {
+        'White': 0,
+        'Black': 1,
+        'Red': 2,
+        'Yellow': 3,
+        'Blue': 4,
+        0: 'White',
+        1: 'Black',
+        2: 'Red',
+        3: 'Yellow',
+        4: 'Blue'
+        }
+
+    def swapColor(self, nameID):
+        return self.colors[nameID]
+
+class LifeMap:
+    def __init__(self, size, colorsSeq,  borderTrough = False):
+        self._size = tuple(size)
+        self.currentColors = Colors()
+        self.connectBorders = borderTrough
+        self.cellMatrix = [[0] * self._size[0]] * self._size[1]
+
+    def getSize(self):
+        return self._size
+
+    def setCell(self, m, n, colorID):
+        self.cellMatrix[m][n] = colorID
+
+    def getCell(self, m, n):
+        return self.cellMatrix[m][n]
+
+    def getCellMatrix(self):
+        return self.cellMatrix
+
+class ColorMap:
+    def __init__(self, lifeMap):
+        self._size = lifeMap.getSize()
+        self.colorMatrix = []
+        for i in range(self._size[1]):
+            self.colorMatrix.append([])
+            for j in range(self._size[0]):
+                self.colorMatrix[i].append(lifeMap.currentColors.swapColor(lifeMap.getCell(i, j)))
+
+    def getSize(self):
+        return self._size
+
+    def getColor(self, m, n):
+        return self.colorMatrix[m][n]
+
+    def getColorMatrix(self):
+        return self.colorMatrix
