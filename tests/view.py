1497085ea47e1d80c4358d7ccc18c88711cba410
Rodion Kostin <itrodion@gmail.com>
2021-06-08 12:03:50 +0300

    test(view): Add bounds draw tests

--- tests/view.py
+++ tests/view.py
@@ -7,13 +7,33 @@ from unittest.mock import MagicMock
 from modules.view import View
 
 class ViewTests(unittest.TestCase):
+  
   def setUp(self):
+    '''Setup tests'''
     controller = MagicMock()
     controller.getLifemapSize = MagicMock(return_value=(50, 50))
 
     self.view = View(controller)
 
-  def test_draw(self):
+  def test_inboundsDraw(self):
+    '''Inbounds draw test'''
+    self.view.controller.getLifemapSize = MagicMock(return_value=(10, 10))
     self.view.showMainWindow()
-    object = self.view.draw(0, 0, 0)
-    print(self.view.cvsCells.itemconfig(object))
+
+    i, j = 1, 2
+    colorIndex = 0
+    object = self.view.draw(i, j, colorIndex)
+
+    CELL_SIZE = self.view.CELL_SIZE
+    x0, y0 = j * CELL_SIZE, i * CELL_SIZE
+    x1, y1 = x0 + CELL_SIZE, y0 + CELL_SIZE
+    self.assertEqual(self.view.cvsCells.coords(object), [x0, y0, x1, y1])
+    
+  def test_outboundsDraw(self):
+    '''Outbounds draw test'''
+    self.view.controller.getLifemapSize = MagicMock(return_value=(10, 10))
+    self.view.showMainWindow()
+    object = self.view.draw(10, 10, 0)
+    self.assertEqual(object, None)
+
+  
