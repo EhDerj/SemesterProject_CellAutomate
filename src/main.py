535a95031119a6ca1eea8caabd49526907aaeb1f
Rodion Kostin <itrodion@gmail.com>
2021-06-08 14:53:37 +0300

    chore

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from modules.view import View
 
 
 if __name__ == '__main__':
-  map = LifeMap((25, 25))
+  map = LifeMap((50, 50))
   manager = RulesNearCells(2, None, True, {})
 
   model = Model(map, manager)
