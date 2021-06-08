a01b400ec604e5c096841ffaa73c141875cb646b
s02180666 <s02180666@prac.cs.msu.ru>
2021-06-07 19:02:11 -0700

    RuleManager docstring documentation improvement

--- CellAutomate/ruleManager.py
+++ CellAutomate/ruleManager.py
@@ -3,7 +3,19 @@ from abc import ABC, abstractmethod
 class RuleManager(ABC):
     """
     Abstract class for creating two offspring rule classes
+
+    Attributes
+    __________
+    ABC:
+        standart abstract class attribute
+
+    Methods
+    _______
+    compute(x, y, lifeMap)
+        abstract method used in RNC.py and RS.py for computing the color at cell (x, y)
+        with consideration of lifeMap settings
     """
+
     @abstractmethod
     def compute(self, x, y, lifeMap):
-        """Abstract method"""
+        """Abstract method for usage in class offsprings"""
