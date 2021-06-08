f491c4cd5a77d12f9e81f1145100f9c8d7ad7f04
eh_derj <endera2001@gmail.com>
2021-06-08 12:47:44 +0300

    Updated Colors class: init updated for support non-constant set of colors

--- src/utils/colors.py
+++ src/utils/colors.py
@@ -1,26 +1,19 @@
 class Colors:
     """
-    Colors class is a basic 5 color palette^ expressed in a dictionary
+    Colors class is a color palette^ expressed in a dictionary
     The correspondence is two-way (for further convenience)
 
     Methods
     _______
+    __init__(colorSeq)
+        >colorSeq: sequence of used collors
+
     swapColor(self, nameID)
         Swaps the numerical ID for <color>: str
     """
-    def __init__(self):
-        self.colors = {
-            'White': 0,
-            'Black': 1,
-            'Red': 2,
-            'Yellow': 3,
-            'Blue': 4,
-            0: 'White',
-            1: 'Black',
-            2: 'Red',
-            3: 'Yellow',
-            4: 'Blue'
-        }
+    def __init__(self, colorSeq):
+        self.colors = { colorSeq[i] : i for i in range(len(colorSeq)) }
+        self.colors.update({ colorSeq[i] : i for i in self.colors })
 
     def swapColor(self, nameID):
         return self.colors[nameID]
