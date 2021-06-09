f387cbb7a1ae41f068c9021237afc654551ad34d
Rodion Kostin <itrodion@gmail.com>
2021-06-09 18:19:37 +0300

    test(controller): Add makeStep test

--- CellAutomate/tests/controller.py
+++ CellAutomate/tests/controller.py
@@ -7,9 +7,17 @@ class ControllerTests(unittest.TestCase):
     """Controller unit tests."""
 
     def setUp(self):
-      model = MagicMock()
+        model = MagicMock()
+        self.model = model
+        self.controller = Controller(model)
 
-      self.model = model
-      #self.controller = Controller(model)
+    def test_makeStep(self):
+        def makeStep():
+          self.model.isStepMade = True
+
+        self.model.isStepMade = False
+        self.model.makeStep = makeStep
+        self.controller.makeStep()
+        self.assertTrue(self.model.isStepMade)
 
 
