3863b97e5520ef1a56da55c9af23cece134ae116
Rodion Kostin <itrodion@gmail.com>
2021-06-09 12:50:31 +0300

    Move main test file to root

---
+++ test.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('./src')
+
+import unittest
+from tests.view import ViewTests
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test.py
+++
@@ -1,5 +0,0 @@
-import unittest
-from view import ViewTests
-
-if __name__ == '__main__':
-    unittest.main()
