3b0d7909eeb884ef0b4aafdf02cf847dfe3643d5
Rodion Kostin <itrodion@gmail.com>
2021-06-09 13:34:35 +0300

    Move locale to main
    Иначе документация не составляется

--- src/main.py
+++ src/main.py
@@ -1,6 +1,18 @@
+"""Main module."""
 from modules.model import Model, LifeMap, RulesNearCells
 from modules.controller import Controller
 from modules.view import View
+import os
+import gettext
+
+ru = gettext.translation(
+    'base',
+    localedir=os.path.dirname("./src/modules/view/ru"),
+    languages=['ru']
+)
+ru.install()
+_ = ru.gettext
+
 
 if __name__ == '__main__':
     map = LifeMap((50, 50))
